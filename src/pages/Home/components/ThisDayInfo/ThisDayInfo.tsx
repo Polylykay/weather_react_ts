78c353fa0211788fd6a09891f36c4ae5477c3b0e
Polylykay <rubtsova-tanyal@mail.ru>
2022-10-13 19:05:23 +0500

    items

--- src/pages/Home/components/ThisDayInfo/ThisDayInfo.tsx
+++ src/pages/Home/components/ThisDayInfo/ThisDayInfo.tsx
@@ -7,14 +7,28 @@ type Props = {}
 
 export const ThisDayInfo = (props: Props) => {
   const items = [{
-    icon_id: '',
-    name: '',
-    value: ''
-  }]
+    icon_id: 'temp',
+    name: ' Температура',
+    value: ' +10° - ошущается как +8°'
+  }, {
+    icon_id: 'pressure',
+    name: 'Давление',
+    value: ' 568 мм ртутного столба - нормальное '
+  }, {
+    icon_id: 'precipitation',
+    name: 'Осадки',
+    value: 'Без осадков'
+  }, {
+    icon_id: 'wind',
+    name: 'Ветер',
+    value: '3 м/с северо-запад - лёгкий ветер'
+  }
+
+  ]
   return (
     <div className={s.this_day_info}>
-          <div className={s.this_day_info_items}> </div>
-          <img src={cloud} alt=''/>
+      <div className={s.this_day_info_items}> </div>
+      <img src={cloud} alt='облако' />
     </div>
   )
 }
